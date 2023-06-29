87863531030a529502e6ed7220fa8d253185d10a
Oleksandr <oleksandr@MacBook-Pro-Oleksandr.local>
2023-06-29 13:02:58 +0300

    add scratch project

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Oleksandr on 29.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
